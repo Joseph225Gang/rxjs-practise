cd1a7622f4de1abb1dd1df5aa8b5b3cfb682ec96
Joseph <lai225joseph@hotmail.com>
2020-01-08 20:05:24 +0800

    shared moudle

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {AutoCompleteComponent} from './auto-complete/auto-complete.component';
 import {LiveSearchComponent} from './live-search/live-search.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 
 
 const routes: Routes = [
   {path:'autocomplete',component:AutoCompleteComponent},
   {path:'liveSearch',component:LiveSearchComponent},
+  {path: '', component:AutoCompleteComponent},
+  {path:'**',component:PageNotFoundComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AutoCompleteComponent } from './auto-complete/auto-complete.component';
-import * as $ from "jquery";
 import { LiveSearchComponent } from './live-search/live-search.component';
+import { LoginModule } from './login/login.module';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     AutoCompleteComponent,
-    LiveSearchComponent
+    LiveSearchComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
+    LoginModule,
     AppRoutingModule
   ],
   providers: [],
